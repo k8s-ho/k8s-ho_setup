4316cf61e771560a083d172f9ea44fa819f51788
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-01-18 16:57:01 +0900

    Update k8s_pkg_env.sh

--- k8s_pkg_env.sh
+++ k8s_pkg_env.sh
@@ -1,73 +1,58 @@
 #!/bin/bash
 
+# Docker, Containerd Setting
 sudo apt-get update
 
-# install docker, containerd
 sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release \
-    apt-transport-https 
+    lsb-release
     
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-sudo echo \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
+  
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo docker version
+
 sudo systemctl enable docker
 sudo systemctl start docker
 
-
+sudo systemctl enable containerd
+sudo systemctl start containerd  
+  
+  
+  
+# Kubernetes Setting
+sudo ufw disable
 sudo swapoff -a && sudo sed -i '/swap/s/^/#/' /etc/fstab
 
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 br_netfilter
 EOF
- 
+
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
+
 sudo sysctl --system
 
 
-
-# install kubernetes
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
 
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
 sudo apt-get update
 sudo apt-get install -y kubelet=$1-00 kubeadm=$1-00 kubectl=$1-00
 sudo apt-mark hold kubelet kubeadm kubectl
 
-
-
-sudo containerd config default > /etc/containerd/config.toml
-
-sudo cat <<EOF > /etc/default/kubelet
-KUBELET_KUBEADM_ARGS=--container-runtime=remote \
-                     --container-runtime-endpoint=/run/containerd/containerd.sock \
-                     --cgroup-driver=systemd
-EOF
-
-sudo cat <<EOF > /etc/crictl.yaml
-runtime-endpoint: unix:///run/containerd/containerd.sock
-image-endpoint: unix:///run/containerd/containerd.sock
-EOF
-
 sudo systemctl daemon-reload
 sudo systemctl restart kubelet
-
-
-sudo echo "192.168.0.100 master-k8sHo" >> /etc/hosts
-for (( i=1; i<=$2; i++  )); do sudo echo "192.168.0.10$i worker_$i-k8sHo" >> /etc/hosts; done
-
-
-sudo cat <<EOF > /etc/resolv.conf
-nameserver 1.1.1.1 #cloudflare DNS
-nameserver 8.8.8.8 #Google DNS
-EOF
-
