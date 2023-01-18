d90f6784319b7e1c9a02d26ebf3b9692fc0aea8d
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-01-18 16:50:29 +0900

    Update k8s_m_setup.sh

--- k8s_m_setup.sh
+++ k8s_m_setup.sh
@@ -9,13 +9,9 @@ cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
 
-apt-install install bash-completion -y
+apt install install bash-completion -y
 kubectl completion bash >/etc/bash_completion.d/kubectl
 
 echo 'alias k=kubectl' >> ~/.bashrc
 echo 'complete -F __start_kubectl k' >> ~/.bashrc
 
-
-git clone https://github.com/k8s-ho/k8s-ho_setup
-mv /home/setup/k8s-ho_setup $HOME
-find $HOME/k8s-ho_setup -regex ".*\.\(sh\)" -exec chmod 700 {} \;
