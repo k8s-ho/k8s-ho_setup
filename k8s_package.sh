7839251e6e07aa187760937380503c3a5fa4268f
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-01-18 13:59:37 +0900

    Update k8s_package.sh
    modi, pkg

--- k8s_package.sh
+++ k8s_package.sh
@@ -1,18 +1,54 @@
 #!/bin/bash
 
-apt update && apt-get install docker-ce docker-ce-cli docker-compose-plugin -y
-apt-get install containerd.io -y 
-apt-get install git -y
+sudo apt-get update
 
+# install docker, containerd
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release \
+    apt-transport-https
+    
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo systemctl enable docker
+sudo systemctl start docker
+
+
+sudo swapoff -a && sudo sed -i '/swap/s/^/#/' /etc/fstab
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+ 
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
+
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+
+
+# install kubernetes
+
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-apt install -y kubelet-$1 kubectl-$1 kubeadm-$1
+sudo apt-get install -y kubelet-$1 kubeadm-$1 kubectl-$1
 sudo apt-mark hold kubelet kubeadm kubectl
 
+
+
+containerd config default > /etc/containerd/config.toml
 
 cat <<EOF > /etc/default/kubelet
 KUBELET_KUBEADM_ARGS=--container-runtime=remote \
@@ -20,13 +56,11 @@ KUBELET_KUBEADM_ARGS=--container-runtime=remote \
                      --cgroup-driver=systemd
 EOF
 
-
 cat <<EOF > /etc/crictl.yaml
 runtime-endpoint: unix:///run/containerd/containerd.sock
 image-endpoint: unix:///run/containerd/containerd.sock
 EOF
 
+sudo systemctl daemon-reload
+sudo systemctl restart kubelet
 
-systemctl enable --now docker
-systemctl enable --now containerd
-systemctl enable --now kubelet
