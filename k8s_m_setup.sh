3d8bad5c23bbe9680285cf0cd94f42517df2a875
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-01-20 01:47:27 +0900

    Update k8s_m_setup.sh
    modi, kubeadm init

--- k8s_m_setup.sh
+++ k8s_m_setup.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 kubeadm init --token 777777.7777777777777777 --token-ttl 0 \
---pod-network-cidr=172.16.0.0/16 --apiserver-advertise-address=192.168.0.100 \
---cri-socket=unix:///run/containerd/containerd.sock
+--apiserver-advertise-address=192.168.0.100 --pod-network-cidr=172.16.0.0/16
 
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
@@ -14,4 +13,3 @@ kubectl completion bash >/etc/bash_completion.d/kubectl
 
 echo 'alias k=kubectl' >> ~/.bashrc
 echo 'complete -F __start_kubectl k' >> ~/.bashrc
-
