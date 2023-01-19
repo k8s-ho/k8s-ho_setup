38959912a004ab3676e7e4d519dfc1c59d24f2dc
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-01-20 01:42:25 +0900

    Update k8s_pkg_env.sh
    modi, containerd

--- k8s_pkg_env.sh
+++ k8s_pkg_env.sh
@@ -7,7 +7,6 @@ sudo apt-get install \
    gnupg \
    lsb-release
 
-sudo swapoff -a && sudo sed -i '/swap/s/^/#/' /etc/fstab
 
 #sudo systemctl stop firewalld
 #sudo systemctl disable firewalld
@@ -25,31 +24,50 @@ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
-#sudo mkdir /etc/docker
+#sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+sudo cat /etc/containerd/config.toml | grep "SystemdCgroup = " | sudo sed 's/false/true/g'
+sudo systemctl restart containerd
 
-cat <<EOF | sudo tee /etc/docker/daemon.json
-{
-   "exec-opts": ["native.cgroupdriver=systemd"],
-   "log-driver": "json-file",
-   "log-opts": {
-      "max-size": "100m"
-   },
-   "storage-driver": "overlay2"
-}
-EOF
+sudo swapoff -a && sudo sed -i '/swap/s/^/#/' /etc/fstab
+# #sudo mkdir /etc/docker
 
-sudo systemctl enable docker
-sudo systemctl start docker
+# cat <<EOF | sudo tee /etc/docker/daemon.json
+# {
+#    "exec-opts": ["native.cgroupdriver=systemd"],
+#    "log-driver": "json-file",
+#    "log-opts": {
+#       "max-size": "100m"
+#    },
+#    "storage-driver": "overlay2"
+# }
+# EOF
+
+# sudo systemctl enable docker
+# sudo systemctl start docker
+
+# tmp test
+# sudo mkdir -p /etc/apt/keyrings
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# sudo apt-get update
+# sudo apt-get install containerd.io
 
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
 br_netfilter
 EOF
 
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward                 = 1
 EOF
 sudo sysctl --system
+
 
 cat <<EOF | sudo tee /etc/resolv.conf
 nameserver 1.1.1.1 
