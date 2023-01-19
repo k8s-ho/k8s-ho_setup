d4c530cc989c1be21531fd888ff7530b2b4f7e39
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-01-19 16:00:06 +0900

    Update k8s_pkg_env.sh
    modi, pkg

--- k8s_pkg_env.sh
+++ k8s_pkg_env.sh
@@ -1,69 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Docker, Containerd Setting
-sudo apt-get update
+swapoff -a && sed -i '/swap/s/^/#/' /etc/fstab
 
-sudo apt-get install -y \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release \
-    git
-    
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-sudo docker version
-
-sudo systemctl enable docker
-sudo systemctl start docker
-
-sudo systemctl enable containerd
-sudo systemctl start containerd  
-  
-  
-  
-# Kubernetes Setting
-sudo ufw disable
-sudo swapoff -a && sudo sed -i '/swap/s/^/#/' /etc/fstab
-
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
-br_netfilter
-EOF
 
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables  = 1
-net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-iptables = 1
 EOF
-
 sudo sysctl --system
 
 
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo ufw disable
 
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
 
 sudo apt-get update
 sudo apt-get install -y kubelet=$1-00 kubeadm=$1-00 kubectl=$1-00
 sudo apt-mark hold kubelet kubeadm kubectl
 
-sudo systemctl daemon-reload
-sudo systemctl restart kubelet
-
-
-sudo echo "192.168.0.100 master-k8sHo" >> /etc/hosts
-for (( i=1; i<=$2; i++  )); do sudo echo "192.168.0.10$i worker_$i-k8sHo" >> /etc/hosts; done
-
-cat <<EOF > /etc/resolv.conf
-nameserver 1.1.1.1 #cloudflare DNS
-nameserver 8.8.8.8 #Google DNS
-EOF
+systemctl daemon-reload
+systemctl restart kubelet
