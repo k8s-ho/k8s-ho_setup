63dce5addc9b3591e2abc66970f004d2ea865112
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-01-19 16:51:07 +0900

    Update k8s_pkg_env.sh

--- k8s_pkg_env.sh
+++ k8s_pkg_env.sh
@@ -11,8 +11,18 @@ sudo sysctl --system
 
 
 sudo ufw disable
-sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli docker-compose-plugin -y
-sudo apt-get install containerd.io -y 
+
+sudo apt-get update
+sudo apt-get install \
+   ca-certificates \
+   curl \
+   gnupg \
+   lsb-release
+   
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+suo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl
 sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
