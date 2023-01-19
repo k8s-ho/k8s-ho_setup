e96894f2bba1bf129d1287b4aad4a2b0207035db
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-01-20 02:53:25 +0900

    Update k8s_pkg_env.sh

--- k8s_pkg_env.sh
+++ k8s_pkg_env.sh
@@ -21,6 +21,7 @@ echo \
 "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+# docker is unnecessary but installed to use
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
