d3733c1bf3897b6eb3964e8dd7b890f4018979bd
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-01-20 01:48:02 +0900

    Update k8s_w_setup.sh

--- k8s_w_setup.sh
+++ k8s_w_setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 kubeadm join --token 777777.7777777777777777 \
-             --discovery-token-unsafe-skip-ca-verification 192.168.0.100:6443 \
-             --cri-socket=unix:///run/containerd/containerd.sock
+             --discovery-token-unsafe-skip-ca-verification 192.168.0.100:6443
