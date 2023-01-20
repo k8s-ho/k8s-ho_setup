27377945e8d7d5733cef47c14fd22efe3700bbd1
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-01-20 15:44:43 +0900

    Update k8s_m_setup.sh

--- k8s_m_setup.sh
+++ k8s_m_setup.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 
-sudo kubeadm init --token 777777.7777777777777777 --apiserver-advertise-address=192.168.0.100 \
---pod-network-cidr=172.16.0.0/16
+sudo kubeadm init --token 777777.7777777777777777 --apiserver-advertise-address=192.168.0.100 --pod-network-cidr=172.16.0.0/16
+
+echo "this is home!!! tmp"
+echo $HOME
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
+echo $(cat $HOME/.kube/config)
+
 sudo apt-get -y install bash-completion
 kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl
 
-echo "alias k=kubectl" >> ~/.bashrc
-echo "complete -o default -F __start_kubectl k" >> ~/.bashrc
-source ~/.bashrc 
+echo "alias k=kubectl" | sudo tee -a $HOME/.bashrc
+echo "complete -o default -F __start_kubectl k" | sudo tee -a $HOME/.bashrc
+source $HOME/.bashrc 
+
+cat $HOME/.bashrc
 
 sudo curl -O https://projectcalico.docs.tigera.io/manifests/calico.yaml 
 sudo sed -i -e 's?192.168.0.0/16?172.16.0.0/16?g' calico.yaml
