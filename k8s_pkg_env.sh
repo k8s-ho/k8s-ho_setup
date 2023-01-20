68b6b38ae451222d6eb8ee6eab9581feda8cbb16
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-01-20 11:16:22 +0900

    Update k8s_pkg_env.sh

--- k8s_pkg_env.sh
+++ k8s_pkg_env.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+dpkg-reconfigure locales
 
 sudo apt-get update
 sudo apt-get install \
