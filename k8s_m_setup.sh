c8c82d7af19bd3c3a2f4b6632f757a094fb2c0bb
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-01-20 11:11:28 +0900

    Update k8s_m_setup.sh

--- k8s_m_setup.sh
+++ k8s_m_setup.sh
@@ -3,7 +3,11 @@
 sudo kubeadm init --token 777777.7777777777777777 --apiserver-advertise-address=192.168.0.100 \
 --pod-network-cidr=172.16.0.0/16
 
-sudo apt-get install bash-completion -y
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+sudo apt-get -y install bash-completion
 kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl
 
 echo "alias k=kubectl" | sudo tee -a ~/.bashrc
@@ -14,6 +18,3 @@ sudo curl -O https://projectcalico.docs.tigera.io/manifests/calico.yaml
 sudo sed -i -e 's?192.168.0.0/16?172.16.0.0/16?g' calico.yaml
 kubectl apply -f calico.yaml
 
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
