96eb91c1742d2ee5e18f0bd6ba19a8485c2cdbb7
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-01-20 11:23:33 +0900

    Update k8s_pkg_env.sh

--- k8s_pkg_env.sh
+++ k8s_pkg_env.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-export LANGUAGE=en_US.UTF-8
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-locale-gen en_US.UTF-8
+
 dpkg-reconfigure locales
 
 cat <<EOF | sudo tee -a /etc/resolv.conf
