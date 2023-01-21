40d5ebf9273a8b5811f693dee78fdeb6527523df
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-01-21 17:53:38 +0900

    Update k8s_m_setup.sh

--- k8s_m_setup.sh
+++ k8s_m_setup.sh
@@ -2,14 +2,12 @@
 
 sudo kubeadm init --token 777777.7777777777777777 --apiserver-advertise-address=192.168.0.100 --pod-network-cidr=172.16.0.0/16
 
-echo "this is home!!! tmp"
 echo $HOME
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-echo $(cat $HOME/.kube/config)
 
 sudo apt-get -y install bash-completion
 kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl
