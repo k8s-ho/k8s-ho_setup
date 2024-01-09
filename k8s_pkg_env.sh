3d11baf329e25443dc17862dd38053c76aef3ab4
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2024-01-09 10:19:15 +0900

    Update k8s_pkg_env.sh

--- k8s_pkg_env.sh
+++ k8s_pkg_env.sh
@@ -83,10 +83,16 @@ sudo sysctl --system
 
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+#sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+#echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+#sudo apt-get update
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list > /dev/null
 sudo apt-get update
-sudo apt-get install -y kubelet=$1-00 kubeadm=$1-00 kubectl=$1-00
+sudo apt-get install -y kubectl kubelet kubeadm
+
+#sudo apt-get install -y kubelet=$1-00 kubeadm=$1-00 kubectl=$1-00
 sudo apt-mark hold kubelet kubeadm kubectl
 
 cat <<EOF > /etc/crictl.yaml
